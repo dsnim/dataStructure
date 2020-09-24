7618adce3f605d34101551b265bde61e0a5058d4
dsnim <57756815+dsnim@users.noreply.github.com>
2020-09-24 13:49:09 +0900

    자료구조 포인터함수
    포인터합수 업데이트

---
+++ dataStructure/01pointer.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+
+void ValueFunc(int a, int b) {
+	a += 10;
+	b += 10;
+}
+
+void Call(int** ppA, int** ppB) {
+	//**ppA = 1;
+}
+
+void ReferenceFunc(int* pA, int* pB) {
+	*pA += 10;
+	*pB += 10;
+	Call(&pA, &pB);
+}
+
+int main() {
+
+	int a = 10;
+	int b = 20;
+
+	ValueFunc(a, b);
+	printf("a : %d, b : %d\n", a, b);
+
+	ReferenceFunc(&a, &b);
+	printf("a : %d, b : %d\n", a, b);
+}
---
+++ dataStructure/03pointer.cpp
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+
+void UpperChar(char* p) {
+	*p += 32;
+}
+
+int InputInt(const char* message) {
+	int value;
+	printf(message);
+	scanf_s("%d", &value);
+	return value;
+}
+
+int main() {
+
+	char stn[] = "Good";
+	UpperChar(stn);
+	printf("%s\n", stn);
+
+	const char* p = "숫자 입력 > ";
+	const char* s = "숫자 입력 > "; //똑같은 상수값은 메모리공간을 만들지 않는다.
+	puts(p);
+	printf("%p, %p\n", p, s);
+
+	int nA = InputInt("데이터 입력 > ");
+	printf("%p\n", &nA);
+	printf("%p\n", stn);
+
+}
