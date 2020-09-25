000c9db6df129bb03f1ea61e8a4922f30a6e6716
dsnim <57756815+dsnim@users.noreply.github.com>
2020-09-25 19:35:31 +0900

    구조체
    구조체

---
+++ dataStructure/09struct.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+
+typedef struct person {
+	char name[20];
+	int age;
+}Person;
+
+typedef struct army {
+	Person basic;
+	char rank[10];
+}Army;
+
+int main() {
+
+	Army man = { "아르미", 20, "병장" };  // man
+											//   |    basic   |  rank  |
+											//   | name | age |
+	printf("이름 : %s\n", man.basic.name);
+	printf("나이 : %d\n", man.basic.age);
+	printf("계급 : %s\n", man.rank);
+
+}
